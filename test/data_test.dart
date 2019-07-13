1f2d5ea6523655469102cf07691f3093f30cd8ec
klejdihoxha <klejdihoxha1998@gmail.com>
2019-07-13 12:30:13 +0200

    First Commit

---
+++ test/data_test.dart
@@ -0,0 +1,24 @@
+import 'package:test/test.dart';
+import 'dart:async';
+import 'package:quizeroo/models/get_data.dart';
+import 'package:quizeroo/models/types.dart';
+import 'package:quizeroo/models/item.dart';
+
+void main() {
+  test('data returns correctly', ()
+  async {
+    _handleTests(value) {
+      expect(value, new isInstanceOf<List>());
+      expect(value.length, 5);
+      expect(value[0].answers, new isInstanceOf<List>());
+      expect(value[0], new isInstanceOf<QuizItem>());
+      expect(value[0].question, new isInstanceOf<String>());
+      expect(value[0].correctAnswer.value, new isInstanceOf<bool>());
+
+      Future _data = new Data(
+          items: 5, difficulty: QuizDifficulty.EASY).getQuestions();
+       _data.then((value) => _handleTests(value));
+    }
+  }
+  );
+}
